466f33b7625e075bcb81b64d71d0c83954512f3d
Agustin Procofio <131788963+AgustinProcofio@users.noreply.github.com>
2024-11-22 16:48:46 -0300

    commandos move y hp

---
+++ src/Library/Commands/HpCommand.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using ClassLibrary;
+
+namespace Library.Commands
+{
+    /// <summary>
+    /// Comando que muestra los HP de los Pokémon de ambos jugadores.
+    /// </summary>
+    public class HpCommand : ModuleBase<SocketCommandContext>
+    {
+        /// <summary>
+        /// Ejecuta el comando 'hp' que muestra los HP de los Pokémon de ambos jugadores.
+        /// </summary>
+        [Command("hp")]
+        [Summary("Muestra los HP de los Pokémon de ambos jugadores.")]
+        public async Task ExecuteAsync()
+        {
+            string displayName = CommandHelper.GetDisplayName(Context);
+            string result = Facade.Instance.GetPokemonsHealth(displayName);
+            await ReplyAsync(result);
+        }
+    }
+}
