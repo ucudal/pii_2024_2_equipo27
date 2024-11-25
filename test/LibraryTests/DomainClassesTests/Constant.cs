04dde306257654564cc2c62e7c4886a1f149e2a2
Avril Viega <avrilviega@gmail.com>
2024-11-25 19:07:01 -0300

    build

--- test/LibraryTests/DomainClassesTests/Constant.cs
+++ test/LibraryTests/DomainClassesTests/Constant.cs
@@ -3,17 +3,17 @@
 /// <summary>
 /// Implementación de IRandom que genera siempre el mismo número.
 /// </summary>
-public class Constant : IRandom
-{
-    private readonly int constantValue;
-
-    public Constant(int value)
-    {
-        constantValue = value;
-    }
-
-    public int Generate()
-    {
-        return constantValue;
-    }
-}
+// public class Constant : IRandom
+// {
+//     private readonly int constantValue;
+//
+//     public Constant(int value)
+//     {
+//         constantValue = value;
+//     }
+//
+//     public int Generate()
+//     {
+//         return constantValue;
+//     }
+// }
