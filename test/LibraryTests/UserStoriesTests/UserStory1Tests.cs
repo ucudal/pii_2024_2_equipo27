b529d0a54d66830bec6b82b0facc0a061987dc51
juanmato <juanmato22@gmail.com>
2024-11-26 20:36:27 -0300

    userstory1test

--- test/LibraryTests/UserStoriesTests/UserStory1Tests.cs
+++ test/LibraryTests/UserStoriesTests/UserStory1Tests.cs
@@ -1,57 +1,50 @@
-using NUnit.Framework;
-namespace ClassLibrary.Tests;
+namespace ClassLibrary.Tests
+{
+    using NUnit.Framework;
+    using ClassLibrary;
+    using System;
 
-public class UserStory1Tests
-{ 
-    // private Facade facade;
-    //
-    // [SetUp]
-    // public void Setup()
-    // {
-    //     facade = new Facade();
-    // }
-    //
-    // [Test]
-    // public voidgi ChoosePokemons_WhenCalled_AddsPokemonsToPlayer()
-    // {
-    //     // Arrange
-    //      pokemonNames = { "Blaziken", "Tinkaton" };
-    //     
-    //     // Act
-    //     string result = facade.ChoosePokemons("1", pokemonNames);
-    //     
-    //     // Assert
-    //     Assert.That(result.Count, Is.EqualTo(1));
-    //     Assert.That(result[0], Is.EqualTo(UserInterface.ShowMessageSelectedPokemons(pokemonNames)));
-    // }
-    //
-    // [Test]
-    // public void ShowMoves_WhenCalled_ReturnsPokemonMoves()
-    // {
-    //     // Arrange
-    //     string[] pokemonNames = {"Blaziken" };
-    //     facade.ChoosePokemons("hola", pokemonNames);
-    //     
-    //     // Act
-    //     List<string> moves = facade.ShowMoves("hola");
-    //     
-    //     // Assert
-    //     Assert.That(moves.Count, Is.GreaterThan(0));
-    //     Assert.That(moves[0], Does.Contain("Blaziken"));
-    // }
-    //
-    // [Test]
-    // public void ChoosePokemonAndMoveToAttack_ActivatesPokemonAndMove()
-    // {
-    //     // Arrange
-    //     string[] pokemonNames = { "Blaziken" };
-    //     facade.ChoosePokemons(1, pokemonNames);
-    //     
-    //     // Act
-    //     facade.ChoosePokemonAndMoveToAttack(1, "Patada Ígnea", "Blaziken");
-    //     
-    //     // Assert
-    //     // Aquí podríamos verificar si el movimiento fue activado correctamente si tuviéramos un método en la clase para comprobar el estado
-    //     Assert.Pass(); // Placeholder - implementar lógica de verificación según la implementación interna.
-    // 
+    [TestFixture]
+    public class UserStory1Tests
+    {
+        [SetUp]
+        public void SetUpPlayersAndBattle()
+        {
+            // Utilizar un patrón similar al ejemplo
+            Facade.Instance.AddPlayerToWaitingList("player1");
+            Facade.Instance.AddPlayerToWaitingList("player2");
+            Facade.Instance.StartBattle("player1", "player2");
+        }
+
+        [Test]
+        public void ChoosePokemons_WhenPlayerDoesNotExist_ShouldReturnErrorMessage()
+        {
+            // Act
+            string result = Facade.Instance.ChoosePokemons("nonExistingPlayer",
+                new[] { "Pikachu", "Larpas" });
+
+            // Assert
+            Assert.That(result, Is.EqualTo("El jugador nonExistingPlayer no está jugando"));
+        }
+
+        [Test]
+        public void ChoosePokemons_WhenPokemonNotInCatalog_ShouldThrowException()
+        {
+            // Act & Assert
+            Assert.That(() => Facade.Instance.ChoosePokemons("player1", new[] { "Pikachu", "PokemonFalso" }),
+                Throws.InstanceOf<Exception>()
+                    .With.Message.EqualTo("El pokemon PokemonFalso no esta en el catalogo"));
+        }
+
+        [Test]
+        public void ChoosePokemons_WhenValidPokemonsSelected_ShouldReturnSuccessMessage()
+        {
+            // Act
+            string message = Facade.Instance.ChoosePokemons("player1",
+                new[] { "Blaziken", "Tinkaton", "Salamence", "Bellsprout", "Zangoose", "Rayquaza" });
+
+            // Assert
+            Assert.That(message, Is.Not.Empty);
+        }
+    }
 }
